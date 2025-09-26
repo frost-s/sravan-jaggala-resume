11c3dc9158857710cb0ba69155cdb25d0d12db5c
frost-s <70838196+frost-s@users.noreply.github.com>
2025-09-26 11:26:43 +0530

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,1 @@
-
-(function(){
-  const btn = document.getElementById('printBtn');
-  if(btn){ btn.addEventListener('click', () => window.print()); }
-})();
+document.getElementById('year').textContent = new Date().getFullYear();
